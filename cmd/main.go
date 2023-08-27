277f134c4cf9f8dff1b96a6861a45639da19bed7
Mehdi Serra <mehdi.serra57@gmail.com>
2023-08-27 17:31:26 +0200

    rename import

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"prayers-time-scraper/internal/scraping"
-	telegram_bot "prayers-time-scraper/internal/telegram-bot"
+	telegrambot "prayers-time-scraper/internal/telegram-bot"
 )
 
 func main() {
 	salatOfTheDay := scraping.GetSalatTime()
-	telegram_bot.SetUpBotConfiguration(salatOfTheDay)
+	telegrambot.SetUpBotConfiguration(salatOfTheDay)
 }
